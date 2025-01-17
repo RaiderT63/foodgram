e3cf14df69a170c5853f462cf34a25a79424c28e
Raider <dimentr631@mail.ru>
2025-01-17 21:31:11 +0400

    Redirect fix v.2

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,15 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
-
-from api.views import short_link_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<int:pk>/', short_link_view, name='short_link'),
+    path('s/<int:id>/', lambda request, id: redirect(
+        f'/recipes/{id}/', permanent=True)
+    ),
 ]
 
 if settings.DEBUG:
