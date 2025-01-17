deb8052fa31d27fb9003852635f2d8e27c527f0c
Raider <dimentr631@mail.ru>
2025-01-17 22:11:20 +0400

    Fix lol

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -127,22 +127,10 @@ DJOSER = {
     },
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost',
     'http://localhost:3000',
-    'http://127.0.0.1',
-    'http://127.0.0.1:3000',
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost',
-    'http://localhost:3000',
-    'http://127.0.0.1',
-    'http://127.0.0.1:3000',
-    'https://foodgram63.ddns.net/',
 ]
 
 # constants
